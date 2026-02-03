b4abb37ca343049daa49af1d8583c9324c9fd455
Rafael305 <fraq86@gmail.com>
2026-02-03 19:15:34 -0300

    primer commit

---
+++ hola_mundo/hola_mundo.cpp
@@ -0,0 +1,12 @@
+#include<iostream> // https://code.visualstudio.com/docs/cpp/config-mingw Este enlace explica cosas interesantes sobre c++
+// #include<vector>
+// #include<string>
+
+using namespace std;
+
+int main(){
+    cout << "Hola mundo" << endl;
+    cout << "desde visual studio code" << '\n';
+    cout << "programando en c++" << endl;
+    return 0;
+}
